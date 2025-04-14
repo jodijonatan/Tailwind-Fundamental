45f7b99fe23b8854318d0cf3c577824abe562cb5
jodijonatan <jdi102350@gmail.com>
2025-04-14 11:36:39 +0700

    used color custom

--- public/index.php
+++ public/index.php
@@ -6,11 +6,11 @@
     <title>TailwindCSS Fundamental</title>
     <link href="/src/css/output.css" rel="stylesheet" />
   </head>
-  <body class="bg-gradient-to-r from-cyan-300 via-cyan-500 to-cyan-600">
+  <body class="bg-gradient-to-r from-[#FBFBFB] via-[#D4F6FF] to-[#C6E7FF]">
     <header class="h-screen">
       <h1 class="text-stone-900 m-5 font-bold">Belajar Tailwind CSS dasar</h1>
       <form action="">
-        <div class="border-2 m-5 p-3 w-80">
+        <div class="border-2 border-[#C6E7FF] m-5 p-3 w-80">
           <label class="font-bold" for="">Nama: </label>
           <input
             class="p-2 outline-none"
@@ -25,14 +25,14 @@
           />
         </div>
         <button
-          class="bg-sky-500 mx-5 px-2 py-1 hover:bg-sky-700 hover:cursor-pointer rounded-[8px]"
+          class="bg-[#9FB3DF] mx-5 px-2 py-1 hover:bg-sky-700 hover:cursor-pointer rounded-[8px]"
         >
           Sign In
         </button>
       </form>
       <div>
         <h3 class="ml-5 mt-1 text-2xl font-bold">Lorem, ipsum.</h3>
-        <p class="mx-5 my-2 p-2 border-1 border-red-500">
+        <p class="mx-5 my-2 p-2 border-1 border-[#C6E7FF]">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
           excepturi tempora assumenda sint. Accusantium itaque accusamus in
           sapiente sed veritatis?
