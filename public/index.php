2c9b3df62d0bf6319d610b23d86f741642b5d605
jodijonatan <jdi102350@gmail.com>
2025-04-15 10:41:38 +0700

    some update

--- public/index.php
+++ public/index.php
@@ -10,13 +10,13 @@
   <nav class="flex justify-between items-center p-4 bg-[#89A8B2] text-stone-800">
     <div class="font-bold mx-15">JO DEV</div>
     <ul class="flex space-x-6 mx-15">
-      <li><a href="/" class="hover:underline">Home</a></li>
+      <li><a href="/public/index.php" class="hover:underline">Home</a></li>
       <li><a href="/public/about.php" class="hover:underline">About</a></li>
       <li><a href="/public/contact.php" class="hover:underline">Contact</a></li>
     </ul>
   </nav>
-  <header>
-    <h1>HOME</h1>
+  <header class="grid grid-flow-col grid-rows-3 gap-4">
+    <h1 class="row-span-2">Salah satu siswan jurusan Rekayasa Perangkat Lunak dari SMK Telkom 1 Medan</h1>
   </header>
 </body>
 </html>
