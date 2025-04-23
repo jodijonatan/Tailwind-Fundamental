374d997d2a44e273dcd849fd0710998ca14e891a
jodijonatan <jdi102350@gmail.com>
2025-04-23 13:46:39 +0700

    some push on github

--- public/main.php
+++ public/main.php
@@ -1,19 +1,35 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="dark">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Testing</title>
     <link href="/src/css/output.css" rel="stylesheet" />
   </head>
-  <di>
-    <div class="overflow-scroll bg-local h-96 bg-[url('https://images.unsplash.com/photo-1593642634367-d91a135587b5')] w-[400px] m-9">
+  <body class="bg-white dark:bg-black">
+    <div class="overflow-scroll bg-local h-96 bg-[url('https://images.unsplash.com/photo-1593642634367-d91a135587b5')] w-[400px] mx-auto mt-5">
       <p class="text-2xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque sint eum magnam ipsa. Nihil inventore dolorem amet repellendus fuga eveniet adipisci incidunt corporis suscipit reiciendis minima ab repellat, nesciunt sapiente odio rerum quibusdam tenetur consequatur provident ullam beatae ratione autem doloribus recusandae. Quae, iusto tempora. Molestiae, tenetur eligendi voluptas harum ipsa saepe maxime hic quod possimus, esse asperiores iure suscipit minima perferendis sed quos earum dolores laboriosam provident aliquam exercitationem? Saepe architecto eum qui error alias pariatur enim aperiam, excepturi veritatis, eos cum! Eius unde earum at, labore neque dolor obcaecati? Iure ducimus, quas aut ipsa quam ab necessitatibus? Natus quibusdam earum qui officiis corporis sequi totam alias cupiditate repudiandae eius aliquam laboriosam minus ut unde rem recusandae, dicta fugiat asperiores iusto. Enim, cum illum. Quod, modi provident explicabo unde tempora saepe tempore vero tenetur qui perferendis ab hic distinctio blanditiis. Praesentium veritatis inventore aperiam consequatur recusandae. Cum esse facere ducimus, consequatur eligendi explicabo nobis vitae soluta tempore minima similique provident adipisci pariatur tenetur dolores alias inventore nulla culpa fugiat architecto molestiae deleniti nemo. A sit architecto illo. Eum blanditiis exercitationem id maxime, at dicta? Dolorum sapiente ad veniam, dignissimos exercitationem maiores, beatae a nostrum cum sit consequatur accusamus repudiandae!</p>
     </div>
+    <hr class="my-10">
+    <div class="p-5 max-w-lg bg-slate-100 rounded-2xl shadow-md border-slate-200 m-auto group hover:bg-sky-800">
+      <h1 class="text-black pt-5 text-[20px] font-semibold group-hover:text-white selection:bg-amber-300 selection:text-black">My Card</h1>
+      <p class="first-line:text-cyan-500 first-letter:text-5xl first-letter:float-left first-letter:mr-1 group-hover:text-white selection:bg-amber-300 selection:text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ducimus deleniti iste iure architecto tempore amet accusamus sed officiis ipsam nihil officia, accusantium velit aliquid recusandae, animi dolore molestias excepturi?</p>
+    </div>
+    <hr class="my-10">
+    <div class="p-5 max-w-lg bg-slate-100 rounded-2xl shadow-md border-slate-200 mx-auto">
+      <form action="">
+        <label for="email">
+          <span class="block font-semibold mb-1 after:content-['*'] after:text-pink-700 after:ml-0.5">Email</span>
+          <input type="email" id="email" placeholder="Masukkan email anda..." class="px-3 py-2 w-full rounded border-slate-500 shadow text-sm placeholder:text-slate-400 focus:outline-none focus:ring-1 focus:ring-sky-700 invalid:text-pink-700 invalid:focus:ring-pink-700 peer">
+          <p class="text-sm m-1 text-pink-700 invisible peer-invalid:visible">Email tidak valid</p>
+        </label>
+      </form>
+    </div>
+    <hr class="my-10">
     <div>
+      <button class="bg-sky-400 py-2 px-5 rounded-full block mx-auto text-white font-semibold hover:bg-sky-600 hover:cursor-pointer active:bg-sky-900 focus:outline-2 focus:outline-stone-950">Slebew</button>
     </div>
-    <div class="mt-96 h-[500px] w-[500px] bg-black m-auto group">
-      <h1 class="text-white text-center pt-5 group-hover:text-amber-300">Lorem, ipsum dolor.</h1>
-    </div>
+    <hr class="my-10">
+    <div>
   </body>
 </html>
